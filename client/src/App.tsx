12a284bb7610fb2736002f401a18fbcb9bb415f1
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2024-06-29 21:42:48 -0400

    commitdocker/k8s

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -44,7 +44,7 @@ const App: React.FC = () => {
 
     try {
       const result = await axios.post(
-        "http://localhost:4000/ingredients",
+        "http://mealgenie.com:4000/ingredients",
         jsonString,
         {
           headers: {
